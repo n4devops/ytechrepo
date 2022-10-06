8859e6fdf66f2564b302d057ed3f05b78ee078a2
n4devops <114721662+n4devops@users.noreply.github.com>
2022-10-06 14:32:11 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -3,7 +3,14 @@ resource "aws_instance" "web" {
  
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
-  user_data = file("userdata.sh")
+   user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install httpd -y
+            sudo systemctl enable httpd
+            sudo systemctl start httpd
+            echo "<h1> Message from `hostname`  </h1>" > /var/www/html/index.html
+            EOF
   tags = {
     Name = "vm1"
     
